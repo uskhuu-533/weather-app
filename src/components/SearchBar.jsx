43b59142776031332bff0c8b80e0364053eeb10a
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2025-01-15 18:58:39 +0800

    1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,6 +12,33 @@ const SearchBar = (props) => {
     setSearchValue(e.target.value);
   };
 
+  const getCountries = async () => {
+   
+    try {
+      const response = await fetch(
+        "https://countriesnow.space/api/v0.1/countries"
+      );
+      const result =await response.json();
+      const data = []
+      for (let i = 0; i < result.data.length; i++) {
+        for (let j = 0; j < result.data[i].cities.length; j++) {
+          const city = result.data[i].cities[j];
+          data.push(city + ", " + result.data[i].country);          
+        }
+      }
+      setCountries(data)
+    } catch (error) {
+      console.log(error);
+    }
+   
+  
+  };
+  useEffect(() => {
+    getCountries();
+  }, []);
+  const handleChangeCity = (city) => {
+    SetSelctedCity(city);
+  };
   useEffect(() => {
     if (searchValue == "") {
       SetData([]);
@@ -23,32 +50,6 @@ const SearchBar = (props) => {
     }
   }, [searchValue]);
   filteredData.length = 4;
-
-  const getCountries = async () => {
-    try {
-      const response = await fetch(
-        "https://countriesnow.space/api/v0.1/countries"
-      );
-      const result = response.json();
-      const data = [];
-      for (let i = 0; i < countriesData.length; i++) {
-        for (let j = 0; j < countriesData[i].cities.length; j++) {
-          const city = countriesData[i].cities[j];
-          data.push(city + ", " + countriesData[i].country);
-        }
-      }
-      setCountries(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    getCountries();
-  });
-  const handleChangeCity = (city) => {
-    SetSelctedCity(city);
-  };
-
   return (
     <div className="fixed mt-[10vh] ml-[15%] w-1/2 ">
       <div className="relative w-3/5 mt-[-70px] flex items-center">
@@ -61,7 +62,7 @@ const SearchBar = (props) => {
         />
       </div>
       {searchValue.length !== 0 ? (
-        <div className="w-3/5 absolute bg-white/80 backdrop-blur-md mt-2 rounded-3xl  ">
+        <div className="w-3/5 absolute bg-white/80 backdrop-blur-md mt-2 rounded-3xl overflow-hidden  ">
           {filteredData.map((el) => (
             <div
               key={el}
